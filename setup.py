79d0dc1b61928fdce330997127b5abb7077f35e1
UchihaDZoro <147326378+UchihaDZoro@users.noreply.github.com>
2024-01-03 15:45:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     windows=['Calc.py', 'startup.py', 'malware1.py', 'malware2.py'],
     options={'py2exe': {'bundle_files': 1, 'compressed': True}},
     zipfile=None,
-    py_modules=['watchdog', 'pytk',]
+    py_modules=['watchdog', 'pytk']
 )
